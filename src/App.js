cd233104f32e93876b08461ff061eee937dfa1a4
raquecre <raquecre@gmail.com>
2023-07-24 23:19:46 +0200

    add alt

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function App() {
                 <td> <img  src={contact.pictureUrl} alt={contact.name} /></td>
                 <td> {contact.name} </td>
                 <td> {contact.popularity} </td>
-                <td>{(contact.wonOscar) ? <img alt="" src='https://as1.ftcdn.net/v2/jpg/03/08/21/18/1000_F_308211869_JZ6kxlkTWHcpjLpkxFrIOLOfptt5Jv2S.jpg' /> : <img src='https://cdn.pixabay.com/photo/2013/07/13/10/27/dislike-157252_1280.png' />}</td>
+                <td>{(contact.wonOscar) ? <img alt="" src='https://as1.ftcdn.net/v2/jpg/03/08/21/18/1000_F_308211869_JZ6kxlkTWHcpjLpkxFrIOLOfptt5Jv2S.jpg' /> : <img alt="" src='https://cdn.pixabay.com/photo/2013/07/13/10/27/dislike-157252_1280.png' />}</td>
                 <td>{(contact.wonEmmy) ? <img alt="" src='https://magnetrononline.com/wp-content/uploads/2018/02/Emmy.jpg' /> : <img alt="" src='https://cdn.pixabay.com/photo/2013/07/13/10/27/dislike-157252_1280.png' />}</td>
                 <td> <button onClick={() => deleteContact(contact)}>Delete</button> </td>
               </tr>
